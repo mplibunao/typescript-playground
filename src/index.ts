5dee748f51286b1e96a8f15bbb86a6e51a7b3acf
MP <markpaololibunao@gmail.com>
2021-04-01 05:27:01 +0800

    - Ignores dist folder - Implements type safe function using generics

--- src/index.ts
+++ src/index.ts
@@ -48,12 +48,12 @@ function fillNum(length: number, value: number): number[] {
   return Array.from({ length }, () => value);
 }
 
-const a = call(fill, 10, "a");
+let a = call(fill, 10, "a");
 console.log("🚀 ~ file: index.ts ~ line 15 ~ a", a);
-const b = call(split, "wat", "");
+let b = call(split, "wat", "");
 console.log("🚀 ~ file: index.ts ~ line 20 ~ b", b);
-const a1 = call(fillNum, 10, 10);
-console.log("🚀 ~ file: index.ts ~ line 42 ~ a1", a1);
+let c = call(fillNum, 10, 10);
+console.log("🚀 ~ file: index.ts ~ line 42 ~ c", c);
 
 // Update call implementation to only work for functions whose second argument is a string. For all other functions, your implementation should fail at compile time
 function updatedCall<T extends [unknown, string, ...unknown[]], R>(
@@ -63,10 +63,10 @@ function updatedCall<T extends [unknown, string, ...unknown[]], R>(
   return f(...args);
 }
 
-const c = updatedCall(fill, 10, "a");
-console.log("🚀 ~ file: index.ts ~ line 15 ~ c", c);
-const d = updatedCall(split, "wat", "");
-console.log("🚀 ~ file: index.ts ~ line 20 ~ d", d);
+let d = updatedCall(fill, 10, "a");
+console.log("🚀 ~ file: index.ts ~ line 15 ~ a", d);
+let e = updatedCall(split, "wat", "");
+console.log("🚀 ~ file: index.ts ~ line 20 ~ b", e);
 // const e = updatedCall(fillNum, 10, 10) // error
 
 // --------------------- can I use rest parameters for object too?
@@ -81,3 +81,29 @@ const reactComponent2 = ({
   [key: string]: string;
 }) => {};
 reactComponent2({ size: "md", color: "blue", type: "string" });
+
+// Implement a small typesafe assertion library, is. Start by sketching out
+// your types. When you’re done, I should be able to use it like this:
+
+// Compare a string and a string
+let f = is("string", "otherstring"); // false
+console.log("🚀 ~ file: index.ts ~ line 90 ~ f", f);
+// Compare a boolean and a boolean
+let g = is(true, false); // false
+console.log("🚀 ~ file: index.ts ~ line 93 ~ g", g);
+
+// Compare a number and a number
+let h = is(42, 42); // true
+console.log("🚀 ~ file: index.ts ~ line 97 ~ h", h);
+
+// Comparing two different types should give a compile-time error
+// is(10, 'foo') // Error TS2345: Argument of type '"foo"' is not assignable
+// to parameter of type 'number'.
+
+// [Hard] I should be able to pass any number of arguments
+let j = is([1], [1, 2], [1, 2, 3]); // false
+console.log("🚀 ~ file: index.ts ~ line 105 ~ j", j);
+
+function is<T>(a: T, ...b: [T, ...T[]]): boolean {
+  return b.every((bItem) => bItem === a);
+}
