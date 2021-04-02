ea87a09452a1995a2ae1372063b69991a1d03969
MP <markpaololibunao@gmail.com>
2021-04-02 17:08:13 +0800

    Adds example of using protected constructor and how it differs with private constructor

--- src/final.ts
+++ src/final.ts
@@ -9,3 +9,16 @@ class MessageQueue {
 
 // Changes the API a bit but Can create an instance
 MessageQueue.create([]);
+
+// Similar to private constructor, protected constructor will not allow you to use new on the class
+// But will allow you to extend from the protected class
+
+class A {
+  protected constructor() {}
+}
+
+class B extends A {} // ok
+// error
+// new A()
+// error
+// new B()
